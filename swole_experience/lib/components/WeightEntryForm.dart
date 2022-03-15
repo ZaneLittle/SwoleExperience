89b857e4ce0aa4905f8ac6069248607830212550
ZaneLittle <zanetlittle@gmail.com>
2022-03-15 15:01:20 -0700

    feat: Add date select for weight entry

--- swole_experience/lib/components/WeightEntryForm.dart
+++ swole_experience/lib/components/WeightEntryForm.dart
@@ -20,16 +20,19 @@ class _WeightEntryFormState extends State<WeightEntryForm> {
   final TextEditingController _weightController = TextEditingController();
   final TextEditingController _dateController = TextEditingController();
 
-  void logWeight(String value) {
-    double? weightVal = double.parse(value);
+  void logWeight() {
+    double? weightVal = double.parse(_weightController.value.text);
+    DateTime dateTime = DateTime.tryParse(_dateController.value.text) != null
+        ? DateTime.parse(_dateController.value.text)
+        : DateTime.now();
 
-    // Close keyboard
+    // Close keyboard and clear fields
     FocusScope.of(context).requestFocus(FocusNode());
-    // Show success
     _weightController.value = TextEditingValue.empty;
+    _dateController.value = TextEditingValue.empty;
 
     WeightService.svc.addWeight(Weight(
-        id: const Uuid().v1(), weight: weightVal, dateTime: DateTime.now()));
+        id: const Uuid().v1(), weight: weightVal, dateTime: dateTime));
 
     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
         content: GestureDetector(
@@ -51,65 +54,85 @@ class _WeightEntryFormState extends State<WeightEntryForm> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: MediaQuery.of(context).size.height *.2,
-        width: MediaQuery.of(context).size.width *.5,
+        height: MediaQuery.of(context).size.height * .2,
+        width: MediaQuery.of(context).size.width * .9,
         padding: const EdgeInsets.only(top: 32),
         child: Form(
             key: _formKey,
             child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-                  // TODO: sizing and hooking up to create
-                  // DateTimeField(
-                  //     format: DateFormat('yyyy-MM-dd HH:mm'),
-                  //     controller: _dateController,
-                  //     decoration: InputDecoration(
-                  //         hintText: DateFormat('yyyy-MM-dd HH:mm')
-                  //             .format(DateTime.now())),
-                  //     onShowPicker: (context, currentValue) async {
-                  //       final date = await showDatePicker(
-                  //           context: context,
-                  //           firstDate: DateTime(1900),
-                  //           initialDate: currentValue ?? DateTime.now(),
-                  //           lastDate: DateTime(2100));
-                  //       if (date != null) {
-                  //         final time = await showTimePicker(
-                  //           context: context,
-                  //           initialTime: TimeOfDay.fromDateTime(
-                  //               currentValue ?? DateTime.now()),
-                  //         );
-                  //         return DateTimeField.combine(date, time);
-                  //       } else {
-                  //         return currentValue;
-                  //       }
-                  //     }),
-                  TextFormField(
-                    // focusNode: _weightEntryFieldFocusListener,
-                    controller: _weightController,
-                    //MediaQuery.of(context).viewInsets.bottom
-                    decoration: InputDecoration(
-                      hintText: 'Enter your weight',
-                      suffixIcon: IconButton(
-                        iconSize: 32,
-                        icon: const Icon(Icons.arrow_circle_right),
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            logWeight(_weightController.value.text);
-                          }
-                        },
-                      ),
-                    ),
-                    keyboardType:
-                        const TextInputType.numberWithOptions(decimal: true),
-                    validator: (String? value) {
-                      return Validator.doubleValidator(value);
-                    },
-                    onFieldSubmitted: (value) {
-                      if (_formKey.currentState!.validate()) {
-                        logWeight(value);
-                      }
-                    },
-                  ),
+                  Row(
+                      // TODO: Elements need better styling and input needs to be hooked up to create
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Flexible(
+                            child: SizedBox(
+                                width: 130,
+                                child: Padding(
+                                    padding: const EdgeInsets.only(left: 4),
+                                    child: DateTimeField(
+                                        format: DateFormat('yyyy-MM-dd HH:mm'),
+                                        controller: _dateController,
+                                        decoration: InputDecoration(
+                                            hintText:
+                                                DateFormat('yyyy-MM-dd HH:mm')
+                                                    .format(DateTime.now())),
+                                        onShowPicker:
+                                            (context, currentValue) async {
+                                          final date = await showDatePicker(
+                                              context: context,
+                                              firstDate: DateTime(1900),
+                                              initialDate: currentValue ??
+                                                  DateTime.now(),
+                                              lastDate: DateTime(2100));
+                                          if (date != null) {
+                                            final time = await showTimePicker(
+                                              context: context,
+                                              initialTime:
+                                                  TimeOfDay.fromDateTime(
+                                                      currentValue ??
+                                                          DateTime.now()),
+                                            );
+                                            return DateTimeField.combine(
+                                                date, time);
+                                          } else {
+                                            return currentValue;
+                                          }
+                                        })))),
+                        Flexible(
+                            child: Padding(
+                                padding: const EdgeInsets.only(right: 4),
+                                child: TextFormField(
+                                  // focusNode: _weightEntryFieldFocusListener,
+                                  controller: _weightController,
+                                  //MediaQuery.of(context).viewInsets.bottom
+                                  decoration: InputDecoration(
+                                    hintText: 'Enter your weight',
+                                    suffixIcon: IconButton(
+                                      iconSize: 32,
+                                      icon:
+                                          const Icon(Icons.arrow_circle_right),
+                                      onPressed: () {
+                                        if (_formKey.currentState!.validate()) {
+                                          logWeight();
+                                        }
+                                      },
+                                    ),
+                                  ),
+                                  keyboardType:
+                                      const TextInputType.numberWithOptions(
+                                          decimal: true),
+                                  validator: (String? value) {
+                                    return Validator.doubleValidator(value);
+                                  },
+                                  onFieldSubmitted: (value) {
+                                    if (_formKey.currentState!.validate()) {
+                                      logWeight();
+                                    }
+                                  },
+                                ))),
+                      ])
                 ])));
   }
 }
