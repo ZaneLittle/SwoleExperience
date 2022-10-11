ad318837d1c1ffc619dc22450ef4fdb1fb7a4f83
ZaneLittle <zanetlittle@gmail.com>
2022-10-10 18:43:16 -0700

    fix: Transparent icon background on some devices

---
+++ swole_experience/lib/constants/pages.dart
@@ -0,0 +1,14 @@
+class Pages {
+
+}
+const String macros = 'macros';
+
+const String weight = 'weight';
+
+const String workout = 'workout';
+
+final Map<String, int> pageMap = {
+  macros: -1, // TODO: Increment each when macros page is implemented
+  weight: 0,
+  workout: 1
+};
