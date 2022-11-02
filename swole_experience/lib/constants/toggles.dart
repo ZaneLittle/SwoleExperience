416765d4fa3bba09674b78712605251ef3b01b83
ZaneLittle <zanetlittle@gmail.com>
2022-11-01 20:55:17 -0700

    feat: Add feature toggles

---
+++ swole_experience/lib/constants/toggles.dart
@@ -0,0 +1,8 @@
+// TODO: Convert to props file that's different for debug vs prod builds
+class Toggles {
+  static const macros = false;
+
+  static const alternativeWorkouts = false;
+
+  static const supersets = false;
+}
